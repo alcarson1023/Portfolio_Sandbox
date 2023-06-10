310e46885c474ec73c7923a51abfa3426cc288e0
unknown <alcarson1023@yahoo.com>
2023-06-10 14:05:02 -0500

    Made changes to PORT configuration in app.py.

--- app.py
+++ app.py
@@ -62,5 +62,9 @@ def test_local():
 
 
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the environment variable PORT if it exists, otherwise use a default port (e.g., 5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
